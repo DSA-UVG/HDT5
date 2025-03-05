924a71ee19b40634cd4a45f01a85bf1f0d33c259
DiegoGuevara2811 <gue24128@uvg.edu.gt>
2025-03-05 11:48:32 -0600

    Update sim.py

--- sim.py
+++ sim.py
@@ -12,5 +12,13 @@ class ProcessSimulator:
         self.cpu = simpy.Resource(env, capacity=num_cpus)
         self.cpu_speed = cpu_speed
         self.process_times = []
-    
+    def run(self, num_processes, interval):
+        def process_generator():
+            for i in range(num_processes):
+                self.env.process(self.process(f'Proceso {i}'))
+                yield self.env.timeout(rd.expovariate(1.0 / interval))
+        
+        self.env.process(process_generator())
+        self.env.run()
+        return self.process_times
    
